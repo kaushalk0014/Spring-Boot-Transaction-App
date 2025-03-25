f10cc32ca548032942a080c30a827734ec8a8755
kaushal kumar <kaushal.k.0014@gmail.com>
2025-03-25 21:19:49 +0530

    initial commit

---
+++ src/main/java/com/transaction/example/SpringBootTransactionAppApplication.java
@@ -0,0 +1,13 @@
+package com.transaction.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootTransactionAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootTransactionAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/transaction/example/SpringBootTransactionAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.transaction.example;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootTransactionAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
